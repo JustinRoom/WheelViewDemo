b7a558082428b0a7ebcd496f8dba048382867b27
justin <1006368252@qq.com>
2018-11-20 19:34:24 +0800

    update demo

--- app/src/main/java/jsc/exam/com/wheelview/fragments/ColumnWheelFragment.java
+++ app/src/main/java/jsc/exam/com/wheelview/fragments/ColumnWheelFragment.java
@@ -25,64 +25,171 @@ import jsc.kit.wheel.dialog.ColumnWheelDialog;
 public class ColumnWheelFragment extends Fragment {
 
     TextView tvResult;
+    ColumnWheelDialog dialog1 = null;
+    ColumnWheelDialog dialog2 = null;
+    ColumnWheelDialog dialog3 = null;
+    ColumnWheelDialog dialog4 = null;
+    ColumnWheelDialog dialog5 = null;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_wheel_dialog, container, false);
         tvResult = root.findViewById(R.id.tv_result);
-        root.findViewById(R.id.btn_choose_time).setOnClickListener(new View.OnClickListener() {
+        root.findViewById(R.id.btn_choose_time_1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showDialog();
+                if (dialog1 == null)
+                    dialog1 = createDialog(1);
+                else
+                    dialog1.show();
+            }
+        });
+        root.findViewById(R.id.btn_choose_time_2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (dialog2 == null)
+                    dialog2 = createDialog(2);
+                else
+                    dialog2.show();
+            }
+        });
+        root.findViewById(R.id.btn_choose_time_3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (dialog3 == null)
+                    dialog3 = createDialog(3);
+                else
+                    dialog3.show();
+            }
+        });
+        root.findViewById(R.id.btn_choose_time_4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (dialog4 == null)
+                    dialog4 = createDialog(4);
+                else
+                    dialog4.show();
+            }
+        });
+        root.findViewById(R.id.btn_choose_time_5).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (dialog5 == null)
+                    dialog5 = createDialog(5);
+                else
+                    dialog5.show();
             }
         });
         return root;
     }
 
-    ColumnWheelDialog<WheelItem, WheelItem, WheelItem, WheelItem, WheelItem> dialog = null;
-
-    private void showDialog() {
-        if (dialog == null) {
-            dialog = new ColumnWheelDialog<>(getActivity());
-            dialog.show();
-            dialog.setTitle("选择菜单");
-            dialog.setCancelButton("取消", null);
-            dialog.setOKButton("确定", new ColumnWheelDialog.OnClickCallBack<WheelItem, WheelItem, WheelItem, WheelItem, WheelItem>() {
-                @Override
-                public boolean callBack(View v, @Nullable WheelItem item0, @Nullable WheelItem item1, @Nullable WheelItem item2, @Nullable WheelItem item3, @Nullable WheelItem item4) {
-                    String result = "";
-                    if (item0 != null)
-                        result += item0.getShowText() + "\n";
-                    if (item1 != null)
-                        result += item1.getShowText() + "\n";
-                    if (item2 != null)
-                        result += item2.getShowText() + "\n";
-                    if (item3 != null)
-                        result += item3.getShowText() + "\n";
-                    if (item4 != null)
-                        result += item4.getShowText() + "\n";
-                    tvResult.setText(result);
-                    return false;
-                }
-            });
-            dialog.setItems(
-                    initItems("菜单选项一"),
-                    initItems("菜单选项二"),
-                    initItems("菜单选项三"),
-                    initItems("菜单选项四"),
-                    initItems("菜单选项五")
-            );
-            dialog.setSelected(
-                    new Random().nextInt(50),
-                    new Random().nextInt(50),
-                    new Random().nextInt(50),
-                    new Random().nextInt(50),
-                    new Random().nextInt(50)
-            );
-        } else {
-            dialog.show();
+    private ColumnWheelDialog createDialog(int type) {
+        ColumnWheelDialog<WheelItem, WheelItem, WheelItem, WheelItem, WheelItem> dialog = new ColumnWheelDialog<>(getActivity());
+        dialog.show();
+        dialog.setTitle("选择菜单");
+        dialog.setCancelButton("取消", null);
+        dialog.setOKButton("确定", new ColumnWheelDialog.OnClickCallBack<WheelItem, WheelItem, WheelItem, WheelItem, WheelItem>() {
+            @Override
+            public boolean callBack(View v, @Nullable WheelItem item0, @Nullable WheelItem item1, @Nullable WheelItem item2, @Nullable WheelItem item3, @Nullable WheelItem item4) {
+                String result = "";
+                if (item0 != null)
+                    result += item0.getShowText() + "\n";
+                if (item1 != null)
+                    result += item1.getShowText() + "\n";
+                if (item2 != null)
+                    result += item2.getShowText() + "\n";
+                if (item3 != null)
+                    result += item3.getShowText() + "\n";
+                if (item4 != null)
+                    result += item4.getShowText() + "\n";
+                tvResult.setText(result);
+                return false;
+            }
+        });
+        switch (type) {
+            case 1:
+                dialog.setItems(
+                        initItems("菜单选项一"),
+                        null,
+                        null,
+                        null,
+                        null
+                );
+                dialog.setSelected(
+                        new Random().nextInt(50),
+                        0,
+                        0,
+                        0,
+                        0
+                );
+                break;
+            case 2:
+                dialog.setItems(
+                        initItems("菜单选项一"),
+                        initItems("菜单选项二"),
+                        null,
+                        null,
+                        null
+                );
+                dialog.setSelected(
+                        new Random().nextInt(50),
+                        new Random().nextInt(50),
+                        0,
+                        0,
+                        0
+                );
+                break;
+            case 3:
+                dialog.setItems(
+                        initItems("菜单选项一"),
+                        initItems("菜单选项二"),
+                        initItems("菜单选项三"),
+                        null,
+                        null
+                );
+                dialog.setSelected(
+                        new Random().nextInt(50),
+                        new Random().nextInt(50),
+                        new Random().nextInt(50),
+                        0,
+                        0
+                );
+                break;
+            case 4:
+                dialog.setItems(
+                        initItems("菜单选项一"),
+                        initItems("菜单选项二"),
+                        initItems("菜单选项三"),
+                        initItems("菜单选项四"),
+                        null
+                );
+                dialog.setSelected(
+                        new Random().nextInt(50),
+                        new Random().nextInt(50),
+                        new Random().nextInt(50),
+                        new Random().nextInt(50),
+                        0
+                );
+                break;
+            case 5:
+                dialog.setItems(
+                        initItems("菜单选项一"),
+                        initItems("菜单选项二"),
+                        initItems("菜单选项三"),
+                        initItems("菜单选项四"),
+                        initItems("菜单选项五")
+                );
+                dialog.setSelected(
+                        new Random().nextInt(50),
+                        new Random().nextInt(50),
+                        new Random().nextInt(50),
+                        new Random().nextInt(50),
+                        new Random().nextInt(50)
+                );
+                break;
         }
+        return dialog;
     }
 
     private WheelItem[] initItems(String label) {
--- app/src/main/java/jsc/exam/com/wheelview/fragments/DateTimeWheelFragment.java
+++ app/src/main/java/jsc/exam/com/wheelview/fragments/DateTimeWheelFragment.java
@@ -12,8 +12,10 @@ import android.widget.TextView;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Random;
 
 import jsc.exam.com.wheelview.R;
+import jsc.kit.wheel.dialog.ColumnWheelDialog;
 import jsc.kit.wheel.dialog.DateTimeWheelDialog;
 
 /**
@@ -26,52 +28,109 @@ import jsc.kit.wheel.dialog.DateTimeWheelDialog;
 public class DateTimeWheelFragment extends Fragment {
 
     TextView tvResult;
+    DateTimeWheelDialog dialog1 = null;
+    DateTimeWheelDialog dialog2 = null;
+    DateTimeWheelDialog dialog3 = null;
+    DateTimeWheelDialog dialog4 = null;
+    DateTimeWheelDialog dialog5 = null;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_wheel_dialog, container, false);
         tvResult = root.findViewById(R.id.tv_result);
-        root.findViewById(R.id.btn_choose_time).setOnClickListener(new View.OnClickListener() {
+        root.findViewById(R.id.btn_choose_time_1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showDialog();
+                if (dialog1 == null)
+                    dialog1 = createDialog(1);
+                else
+                    dialog1.show();
+            }
+        });
+        root.findViewById(R.id.btn_choose_time_2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (dialog2 == null)
+                    dialog2 = createDialog(2);
+                else
+                    dialog2.show();
+            }
+        });
+        root.findViewById(R.id.btn_choose_time_3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (dialog3 == null)
+                    dialog3 = createDialog(3);
+                else
+                    dialog3.show();
+            }
+        });
+        root.findViewById(R.id.btn_choose_time_4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (dialog4 == null)
+                    dialog4 = createDialog(4);
+                else
+                    dialog4.show();
+            }
+        });
+        root.findViewById(R.id.btn_choose_time_5).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (dialog5 == null)
+                    dialog5 = createDialog(5);
+                else
+                    dialog5.show();
             }
         });
         return root;
     }
 
-    DateTimeWheelDialog dialog = null;
+    private DateTimeWheelDialog createDialog(int type) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, 2015);
+        calendar.set(Calendar.MONTH, 0);
+        calendar.set(Calendar.DAY_OF_MONTH, 1);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        Date startDate = calendar.getTime();
+        calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, 2020);
+        Date endDate = calendar.getTime();
 
-    private void showDialog() {
-        if (dialog == null) {
-            Calendar calendar = Calendar.getInstance();
-            calendar.set(Calendar.YEAR, 2015);
-            calendar.set(Calendar.MONTH, 0);
-            calendar.set(Calendar.DAY_OF_MONTH, 1);
-            calendar.set(Calendar.HOUR_OF_DAY, 0);
-            calendar.set(Calendar.MINUTE, 0);
-            Date startDate = calendar.getTime();
-            calendar = Calendar.getInstance();
-            calendar.set(Calendar.YEAR, 2020);
-            Date endDate = calendar.getTime();
-
-            dialog = new DateTimeWheelDialog(getActivity());
-            dialog.show();
-            dialog.setTitle("选择时间");
-            dialog.configShowUI(DateTimeWheelDialog.SHOW_YEAR_MONTH_DAY_HOUR_MINUTE);
-            dialog.setCancelButton("取消", null);
-            dialog.setOKButton("确定", new DateTimeWheelDialog.OnClickCallBack() {
-                @Override
-                public boolean callBack(View v, @NonNull Date selectedDate) {
-                    tvResult.setText(SimpleDateFormat.getInstance().format(selectedDate));
-                    return false;
-                }
-            });
-            dialog.setDateArea(startDate, endDate, true);
-            dialog.updateSelectedDate(new Date());
-        } else {
-            dialog.show();
+        DateTimeWheelDialog dialog = new DateTimeWheelDialog(getActivity());
+        dialog.show();
+        dialog.setTitle("选择时间");
+        int config = DateTimeWheelDialog.SHOW_YEAR_MONTH_DAY_HOUR_MINUTE;
+        switch (type) {
+            case 1:
+                config = DateTimeWheelDialog.SHOW_YEAR;
+                break;
+            case 2:
+                config = DateTimeWheelDialog.SHOW_YEAR_MONTH;
+                break;
+            case 3:
+                config = DateTimeWheelDialog.SHOW_YEAR_MONTH_DAY;
+                break;
+            case 4:
+                config = DateTimeWheelDialog.SHOW_YEAR_MONTH_DAY_HOUR;
+                break;
+            case 5:
+                config = DateTimeWheelDialog.SHOW_YEAR_MONTH_DAY_HOUR_MINUTE;
+                break;
         }
+        dialog.configShowUI(config);
+        dialog.setCancelButton("取消", null);
+        dialog.setOKButton("确定", new DateTimeWheelDialog.OnClickCallBack() {
+            @Override
+            public boolean callBack(View v, @NonNull Date selectedDate) {
+                tvResult.setText(SimpleDateFormat.getInstance().format(selectedDate));
+                return false;
+            }
+        });
+        dialog.setDateArea(startDate, endDate, true);
+        dialog.updateSelectedDate(new Date());
+        return dialog;
     }
 }
